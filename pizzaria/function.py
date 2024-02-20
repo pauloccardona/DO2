e4f79034c16f4b8de74c191e502a250a729bf0d1
pauloccardona <paulo.cesar.cardona@gmail.com>
2024-02-20 18:38:37 +0000

    type error function.py

--- pizzaria/function.py
+++ pizzaria/function.py
@@ -1,9 +1,9 @@
 import json
 
-def attemp_json_deserialize(data, expect_type=none):
+def attemp_json_deserialize(data, expect_type=None):
     try:
         data = json.loads(data)
-    expect(TypeError, json.decoder.JSONDecodeError): pass
+    except(TypeError, json.decoder.JSONDecodeError): pass
 
     if expect_type is not None and not isinstance(data, expect_type):
         raise ValueError(f"Got {type(data)} but expected {expect_type}.")
